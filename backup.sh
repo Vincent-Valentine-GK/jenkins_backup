db5af2f87f0cd3ff742d5b2160067a3cb4a58bf3
Vincent Valentine <vincent_valentine@outlook.com>
2020-12-10 23:24:57 -0600

    Test run4.

--- backup.sh
+++ backup.sh
@@ -4,25 +4,23 @@ whoami
 echo "We are HEREEEEEEEEEEEEEE!"
 pwd
 cd "/var/jenkins_home/"
-git init
-git remote add origin git@gitlab.com:DrunkinMastaRay_Orchestration/jenkins_backup.git
 pwd 
 ls -la | grep TEST
 ls -la | grep git
 echo "Before git!!!!!!!!!!!!!!!!!!"
 # Add general configurations, job configurations, and user content
-git add -- *.xml jobs/*/*.xml userContent/* ansible/*
+sudo git add -- *.xml jobs/*/*.xml userContent/* ansible/*
 # only add user configurations if they exist
 if [ -d users ]; then
     user_configs=`ls users/*/config.xml`
 if [ -n "$user_configs" ]; then
-        git add $user_configs
+        sudo git add $user_configs
 fi
 fi
 # mark as deleted anything that's been, well, deleted
 to_remove=`git status | grep "deleted" | awk '{print $3}'`
 if [ -n "$to_remove" ]; then
-    git rm --ignore-unmatch $to_remove
+    sudo git rm --ignore-unmatch $to_remove
 fi
-git commit -m "Automated Jenkins commit"
-git push -q -u origin master
+sudo git commit -m "Automated Jenkins commit"
+sudo git push -q -u origin master
